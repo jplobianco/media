6ebfb9c1a0e3a7addb03d2592acb074fd5a87533
João Paulo Lobianco <joaopaulolobianco1@gmail.com>
2023-05-22 21:35:43 -0300

    wip

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -1,20 +1,33 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { faker } from "@faker-js/faker";
 
 const albumsApi = createApi({
-  reducerPath: 'albums',
+  reducerPath: "albums",
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:3005',
+    baseUrl: "http://localhost:3005",
   }),
   endpoints(builder) {
     return {
+      addAlbum: builder.mutation({
+        query: (user) => {
+          return {
+            url: "/albums",
+            method: "POST",
+            body: {
+              userId: user.id,
+              title: faker.commerce.productName(),
+            },
+          };
+        },
+      }),
       fetchAlbums: builder.query({
         query: (user) => {
           return {
-            url: '/albums',
+            url: "/albums",
             params: {
               userId: user.id,
             },
-            method: 'GET',
+            method: "GET",
           };
         },
       }),
@@ -22,5 +35,5 @@ const albumsApi = createApi({
   },
 });
 
-export const { useFetchAlbumsQuery } = albumsApi;
+export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
 export { albumsApi };
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { setupListeners } from '@reduxjs/toolkit/query';
-import { usersReducer } from './slices/usersSlice';
-import { albumsApi } from './apis/albumsApi';
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { usersReducer } from "./slices/usersSlice";
+import { albumsApi } from "./apis/albumsApi";
 
 export const store = configureStore({
   reducer: {
@@ -15,7 +15,7 @@ export const store = configureStore({
 
 setupListeners(store.dispatch);
 
-export * from './thunks/fetchUsers';
-export * from './thunks/addUser';
-export * from './thunks/removeUser';
-export { useFetchAlbumsQuery } from './apis/albumsApi';
+export * from "./thunks/fetchUsers";
+export * from "./thunks/addUser";
+export * from "./thunks/removeUser";
+export { useFetchAlbumsQuery, useAddAlbumMutation } from "./apis/albumsApi";
